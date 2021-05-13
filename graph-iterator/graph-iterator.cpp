04f8777df5dea28afbc0262c30544509ab0df974
mdpt <mada.patrik@gmail.com>
2021-05-13 16:32:05 +0200

    feat: Add Iterator implementation.

--- graph-iterator/graph-iterator.cpp
+++ graph-iterator/graph-iterator.cpp
@@ -1,10 +1,35 @@
-﻿// graph-iterator.cpp : Defines the entry point for the application.
+﻿#include <iostream>
 
-#include "graph.h"
-#include "graph-iterator.h"
+#include "logic/bfs-iterator.h"
+#include "logic/dfs-iterator.h"
+
+void iterate(DFSIterator iterator) {
+
+	iterator.reset();
+
+	while ( !iterator.isEnd() ) {
+		std::cout << iterator.currentKey() << " ";
+		iterator.next();
+	}
+}
 
 int main(int argc, char* argv[]) {
-	Graph graph = Graph("graph1.txt");
-	graph.printGraph();
+
+	try {
+		Graph graph{ "graph1.txt" };
+		graph.printAdjacencyMatrix();
+
+		DFSIterator dfs{ graph };
+		std::cout << "Info: DFSIterator initialized.\n";
+		iterate(dfs);
+
+		BFSIterator bfs{ graph };
+		//iterate(bfs);
+
+	} catch (std::string msg) {
+		std::cout << msg;
+		return 1;
+	}
+
 	return 0;
 }
